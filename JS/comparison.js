1e8fc4c487f1ee2e3ca34031c5f296cb2221cad1
Justin <apolongjustin49@gmail.com>
2025-12-07 20:55:26 +0800

    added a new file

---
+++ JS/comparison.js
@@ -0,0 +1,19 @@
+/*
+  * Comparison Operators
+  * Comparison operators are used to compare two values. 
+  * Comparison operators always return true or false.
+  
+  * Given that x = 5, the table below explains the comparison operators:
+
+  * Operator     Description	                      Comparing  	    Returns	
+  -    ==     |  equal to                       |   x == 8     |     false
+  -    ===    |  equal value and equal type     |   x === 5    |     true
+  -    !=     |  not equal                      |   x != 8     |     true
+  -    !==    |  not equal value or equal type  |   x !== 5    |     false
+  -    >      |  greater than                   |   x > 8      |     false
+  -    <      |  less than                      |   x < 3      |     true
+  -    >=     |  greater than or equal to       |   x >= 5     |     true
+  -    <=     |  less than or equal to          |   x <= 6     |     false
+
+  !Note: All comparison operators above can be used on strings, though it is compared alphabetically.
+*/
