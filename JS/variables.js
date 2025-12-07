483383ff27d727ad7b74eef46c8d55cfc9337928
Justin <apolongjustin49@gmail.com>
2025-12-07 15:12:18 +0800

    almost finishing the variable.js file

---
+++ JS/variables.js
@@ -0,0 +1,15 @@
+/*
+  * Variables = Data Containers
+  * JavaScript variables are containers for data.
+  * JavaScript variables can be declared in 4 ways:
+
+  * Modern JS
+  - Using let
+  - Using const
+
+  * Older JS
+  - Using var (Not Recommended)
+  - Automatically (Not Recommended)
+*/
+
+//
