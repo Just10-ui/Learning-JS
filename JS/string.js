fe16edc70a544770f4a795fc6c2cbd299d28655a
Justin <apolongjustin49@gmail.com>
2025-12-12 13:53:52 +0800

    added a new file

---
+++ JS/string.js
@@ -0,0 +1,37 @@
+// * strings are for storing text
+
+// example of using strings
+let name = 'Justin';
+
+// * 3 ways of creating a strings
+
+// - using single quote
+let single = 'single';
+
+// - using double quote
+let double = "double";
+
+// - using backtic
+let back = `back`;
+
+/*
+ * Code	     Result	      Description
+ -  \'	 |     '	    |   Single quote
+ -  \"	 |     "	    |   Double quote
+ -  \\	 |     \	    |   Backslash
+*/
+
+console.log('"vikings"'); // output "vikings"
+console.log("\"vikings\""); // output "vikings"
+console.log('\'vikings\''); // output 'vikings'
+console.log("'vikings'"); //output 'vikings'
+
+/*
+  *   Code	           Result
+  -    \b	     |       Backspace
+  -    \f	     |       Form Feed
+  -    \n	     |       New Line
+  -    \r	     |       Carriage Return
+  -    \t	     |       Horizontal Tabulator
+  -    \v	     |       Vertical Tabulator 
+*/
