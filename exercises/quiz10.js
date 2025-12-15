c59de1a46d9dfa2be0fc4ef895c3bf4c43364e72
Justin <apolongjustin49@gmail.com>
2025-12-15 17:13:42 +0800

    finished my 10th quiz

---
+++ exercises/quiz10.js
@@ -0,0 +1,8 @@
+//* easy
+//? Create an object called movie with the following properties:
+const movie = {
+  title: 'Ang alamat ni nash',
+  director: 'francine',
+  releaseYear: 2022,
+}
+console.log(movie.director);
